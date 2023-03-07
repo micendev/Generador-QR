536d37115583eb8d06e03054520b62035edcc4f5
Michael <michael.enriquez.mo@gmail.com>
2023-03-07 17:13:06 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper");
+qrInput = wrapper.querySelector(".form input");
+generateBtn = wrapper.querySelector(".form button");
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generando código QR...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generar código QR";
+
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
